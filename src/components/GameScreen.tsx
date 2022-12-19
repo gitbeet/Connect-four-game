b128938b397fe9e9fafd1bdd7f2d8d79ece8a3ae
gitbeet <nikig715@gmail.com>
2022-12-19 12:33:14 +0100

    working

--- src/components/GameScreen.tsx
+++ src/components/GameScreen.tsx
@@ -12,7 +12,7 @@ const GameScreen = () => {
     useGameContext();
   return (
     <>
-      {!isGameStarted && !winnerDecided && <StartGame />}
+      <StartGame />
       <Header />
       <Timer />
       <BoardSection />
--- src/components/StartGame.tsx
+++ src/components/StartGame.tsx
@@ -1,25 +1,34 @@
 import { useGameContext } from "../context/gameContext";
+import { motion, AnimatePresence } from "framer-motion";
+import { modalVariants } from "../variants";
 import Backdrop from "./Backdrop";
-import Button from "./Button";
 
 const StartGame = () => {
-  const { setIsGameStarted } = useGameContext();
+  const { setIsGameStarted, isGameStarted, winnerDecided } = useGameContext();
   return (
-    <>
-      <div
-        className="absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[40]
+    <AnimatePresence>
+      {!isGameStarted && !winnerDecided ? (
+        <>
+          <motion.div
+            variants={modalVariants}
+            initial="hidden"
+            animate="visible"
+            exit="close"
+            className="absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[40]
 w-[min(90%,400px)]"
-      >
-        <div
-          onClick={() => setIsGameStarted(true)}
-          className="bg-blue-400 hover:bg-blue-300 select-none cursor-pointer text-white border-[3px] border-outline text-4xl text-center font-numbers  px-8 py-6  after:bg-outline after:absolute after:w-full after:h-full after:top-2 after:left-2 after:z-[-1] transition-all duration-150  before:border-t-2 before:border-l-2 before:border-white before:absolute before:top-[3px] before:left-[3px] before:right-[3px] before:bottom-[3px]"
-        >
-          {/* <Button text="Press To Start" onClick={() => setIsGameStarted(true)} /> */}
-          Press to start
-        </div>
-      </div>
-      <Backdrop />
-    </>
+          >
+            <div
+              onClick={() => setIsGameStarted(true)}
+              className="bg-blue-400 hover:bg-blue-300 select-none cursor-pointer text-white border-[3px] border-outline text-4xl text-center font-numbers  px-8 py-6  after:bg-outline after:absolute after:w-full after:h-full after:top-2 after:left-2 after:z-[-1] transition-all duration-150  before:border-t-2 before:border-l-2 before:border-white before:absolute before:top-[3px] before:left-[3px] before:right-[3px] before:bottom-[3px]"
+            >
+              {/* <Button text="Press To Start" onClick={() => setIsGameStarted(true)} /> */}
+              Press to start
+            </div>
+          </motion.div>
+          <Backdrop />
+        </>
+      ) : null}
+    </AnimatePresence>
   );
 };
 
